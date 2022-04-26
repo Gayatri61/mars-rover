0b57fca67ceb7b8238838b5700238478eb6848ad
gayat <gayat@Gayatri27>
2022-04-26 21:38:37 +0530

    Negative Test cases

--- src/test/java/com/gayatri/marsrover/service/impl/MarsRoverServiceTest.java
+++ src/test/java/com/gayatri/marsrover/service/impl/MarsRoverServiceTest.java
@@ -1,6 +1,7 @@
 package com.gayatri.marsrover.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,6 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.gayatri.marsrover.dto.MarsRoverRequest;
 import com.gayatri.marsrover.dto.MarsRoverResponse;
+import com.gayatri.marsrover.exception.InvalidInputException;
 
 @ExtendWith(MockitoExtension.class)
 public class MarsRoverServiceTest {
@@ -27,6 +29,30 @@ public class MarsRoverServiceTest {
 		MarsRoverResponse response=marsRoverServiceImpl.marsRoverFunction(marsRoverRequest);
 		marsRoverExpectedResponse=new MarsRoverResponse(1, 3, "N");
 		assertEquals(marsRoverExpectedResponse.toString(), response.toString());
+	}
+	
+	@Test()
+	public void testMarsRoverFunction_InvalidDirection() {
+		marsRoverRequest=new MarsRoverRequest(5, 5, 1, 2, null, "LMLMLMLMM");
+		assertThrows(InvalidInputException.class, ()->{marsRoverServiceImpl.marsRoverFunction(marsRoverRequest);});
+		
+		marsRoverRequest=new MarsRoverRequest(5, 5, 1, 2, "", "LMLMLMLMM");
+		assertThrows(InvalidInputException.class, ()->{marsRoverServiceImpl.marsRoverFunction(marsRoverRequest);});
+		
+		marsRoverRequest=new MarsRoverRequest(5, 5, 1, 2, "K", "LMLMLMLMM");
+		assertThrows(InvalidInputException.class, ()->{marsRoverServiceImpl.marsRoverFunction(marsRoverRequest);});
+	}
+	
+	@Test()
+	public void testMarsRoverFunction_InvalidInstruction() {
+		marsRoverRequest=new MarsRoverRequest(5, 5, 1, 2, "N", null);
+		assertThrows(InvalidInputException.class, ()->{marsRoverServiceImpl.marsRoverFunction(marsRoverRequest);});
+		
+		marsRoverRequest=new MarsRoverRequest(5, 5, 1, 2, "N", "");
+		assertThrows(InvalidInputException.class, ()->{marsRoverServiceImpl.marsRoverFunction(marsRoverRequest);});
+		
+		marsRoverRequest=new MarsRoverRequest(5, 5, 1, 2, "N", "KKKKKKKK");
+		assertThrows(InvalidInputException.class, ()->{marsRoverServiceImpl.marsRoverFunction(marsRoverRequest);});
 	}
 
 }
